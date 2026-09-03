d0ed1224f736229a577d58395afebe6569cc9a47
awille-zhdk <awille@zhdk.ch>
2026-09-03 10:13:45 +0200

    Movement stuff

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,27 +12,99 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         layerMask = LayerMask.GetMask("Floor");
+        AdhereToSurface();
     }
 
     // Update is called once per frame
     void Update()
     {
-        AdhereToSurface();
-        transform.Translate(new Vector3(moveValue.x, 0, moveValue.y) * moveSpeed * Time.deltaTime);
+        AdhereMovement();
+        //transform.Translate(new Vector3(moveValue.x, 0, moveValue.y) * moveSpeed * Time.deltaTime);
+        if (moveValue.x > 0)
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (moveValue.y != 0)
+        {
+            transform.Rotate(new Vector3(0, moveValue.y, 0));
+        }
         if (moveValue != Vector2.zero)
         {
-            SpawnGrass();
+            //SpawnGrass();
+        }
+        if (!HasGround())
+        {
+            Debug.Log("Ha");
+            AdhereToSurface();
         }
     }
 
     private void AdhereToSurface()
     {
         RaycastHit hit;
-        Vector3 raycastPosition = new Vector3(transform.position.x, transform.position.y + 0.3f, transform.position.z);
-        if (Physics.Raycast(raycastPosition, -transform.up, out hit, Mathf.Infinity))
+        Vector3 raycastPosition = transform.TransformPoint(0, 0.6f, 0);
+        if (Physics.Raycast(raycastPosition, -transform.up, out hit, 1.5f))
         {
+            Debug.DrawLine(raycastPosition, hit.point, Color.purple, 1f);
+            //transform.position = new Vector3(hit.point.x, hit.point.y + 0.1f, hit.point.z);
             transform.position = hit.point;
         }
+    }
+
+    private void AdhereMovement()
+    {
+        float savedYRotation = transform.localEulerAngles.y;
+        RaycastHit hit;
+        Vector3 raycastNormal;
+        Vector3 raycastPosition = transform.position;
+        for (int i = 1; i < 4; i++)
+        {
+            Debug.DrawRay(raycastPosition, transform.forward * (0.2f * i), Color.blue, 2f);
+            if (!HasGround())
+            {
+                Debug.DrawRay(raycastPosition + transform.forward * (0.2f * i), -transform.up * (0.2f * i), Color.blue, 2f);
+                Debug.DrawRay(raycastPosition + (transform.forward * (0.2f * i)) - (transform.up * (0.2f * i)), -transform.forward * (0.8f * i), Color.blue, 2f);
+            }
+            if (Physics.Raycast(raycastPosition, transform.forward, out hit, 0.2f * i))
+            {
+                raycastNormal = hit.normal;
+                transform.up = raycastNormal;
+                transform.position = hit.point;
+                break;
+
+                //AdhereToSurface();
+            }
+            else if (Physics.Raycast(raycastPosition + transform.forward * (0.2f * i), -transform.up, out hit, 0.2f * i) && !HasGround())
+            {
+                raycastNormal = hit.normal;
+                transform.up = raycastNormal;
+                Vector3 newRotation = transform.localEulerAngles;
+                newRotation.y = savedYRotation;
+                transform.localEulerAngles = newRotation;
+                transform.position = hit.point;
+                break;
+                //AdhereToSurface();
+            }
+            else if (Physics.Raycast(raycastPosition + (transform.forward * (0.2f * i)) - (transform.up * (0.2f * i)), -transform.forward, out hit, 0.8f * i) && !HasGround())
+            {
+                raycastNormal = hit.normal;
+                transform.up = raycastNormal;
+                Vector3 newRotation = transform.localEulerAngles;
+                newRotation.y = savedYRotation;
+                transform.localEulerAngles = newRotation;
+                transform.position = hit.point;
+                break;
+                //AdhereToSurface();
+            }
+        }
+    }
+
+    private bool HasGround()
+    {
+        Vector3 raycastPos = transform.TransformPoint(0, 0.1f, 0);
+        Debug.DrawRay(raycastPos, -transform.up * 0.3f, Color.green, 1f);
+        return Physics.Raycast(raycastPos, -transform.up, 0.3f);
     }
 
     private void SpawnGrass()
